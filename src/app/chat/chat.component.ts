0b9c61cb80cfdeacef2a8db61a4c0240c73106be
Gonza Rico <gonzaricoesteban@gmail.com>
2025-03-07 13:17:04 +0100

    home page as a hub

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-chat',
+  imports: [RouterLink],
+  templateUrl: './chat.component.html',
+})
+export class ChatComponent {
+
+}
